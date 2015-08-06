b6609cbd93aceb7adddfad208b2b16ec1f784c1c
François <briatte@gmail.com>
2015-08-06 18:41:56 +0200

    use raw data if found zipped

--- make.r
+++ make.r
@@ -6,8 +6,13 @@ source("parties.r")
 
 # folders
 
-dir.create("data"   , showWarnings = FALSE)
-dir.create("plots"  , showWarnings = FALSE)
+dir.create("data"  , showWarnings = FALSE)
+dir.create("plots" , showWarnings = FALSE)
+
+if (file.exists("photos.zip"))
+  unzip("photos.zip")
+
+dir.create("photos", showWarnings = FALSE)
 
 if (file.exists("raw.zip"))
   unzip("raw.zip")
@@ -17,11 +22,6 @@ dir.create("raw/bill-lists" , showWarnings = FALSE)
 dir.create("raw/bill-pages" , showWarnings = FALSE)
 dir.create("raw/mp-pages"   , showWarnings = FALSE)
 
-if (file.exists("photos.zip"))
-  unzip("photos.zip")
-
-dir.create("photos" , showWarnings = FALSE)
-
 # parameters
 
 plot = TRUE
@@ -29,9 +29,9 @@ gexf = TRUE
 mode = "fruchtermanreingold"
 meta = c(
   "cty" = "Romania",
-  "lang" = "ro", # Wikipedia language for chamber and constituencies
-  "ca" = "Camera_Deputaților_din_România",
-  "se" = "Senatul_României",
+  "lang" = "en", # Wikipedia language for chamber and constituencies
+  "ca" = "Chamber_of_Deputies_(Romania)",
+  "se" = "Senate_of_Romania",
   "type-ca" = "Lower",
   "type-se" = "Upper",
   "ipu-ca" = 2261,
